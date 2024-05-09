435c1d9f2a61867017c6fbe8b428503a071de769
mzm <mzm-0571@mail.ustc.edu.cn>
2024-05-09 15:53:33 +0800

    Second submit

---
+++ src/com/awt/AwtTry01.java
@@ -0,0 +1,15 @@
+package com.awt;
+
+import java.awt.*;
+
+/**
+ * @author mzm
+ * @version 1.0
+ */
+public class AwtTry01 {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setSize(500,300);
+        frame.setVisible(true);
+    }
+}
